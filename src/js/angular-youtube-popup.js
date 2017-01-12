1ac538ffb44d54a10df9e8b875f73e2f76c5bf8f
Sanchit <dev@minutephp.com>
2017-01-12 09:57:17 +0530

    updated to version v0.0.1

---
+++ src/js/angular-youtube-popup.js
@@ -0,0 +1,57 @@
+/// <reference path="../../../minute/_all.d.ts" />
+var Minute;
+(function (Minute) {
+    var AngularYouTubePopup = (function () {
+        function AngularYouTubePopup() {
+            var _this = this;
+            this.$get = function ($rootScope, youtubeEmbedUtils, $ui) {
+                var service = {};
+                var template = "\n                <div class=\"box box-lg\" ng-init=\"data.loadVideo()\">\n                    <div class=\"box-header with-border\">\n                        <b class=\"pull-left\"><span translate=\"\">{{data.title || 'Video player'}}</span></b>\n                        <a class=\"pull-right close-button\" href=\"\"><i class=\"fa fa-times\"></i></a>\n                        <div class=\"clearfix\"></div>\n                    </div>\n        \n                    <form class=\"form-horizontal\">\n                        <div class=\"box-body\">\n                            <div class=\"embed-responsive embed-responsive-16by9\">\n                                 <youtube-video class=\"embed-responsive-item\" video-url=\"data.videoUrl\" player-vars=\"data.playerVars\"></youtube-video>\n                            </div>\n                        </div>\n                    </form>\n                </div>\n            ";
+                service.popup = function (videoUrl, title, modal, playerVars) {
+                    if (title === void 0) { title = ''; }
+                    if (modal === void 0) { modal = false; }
+                    if (playerVars === void 0) { playerVars = { autoplay: 1, controls: 0, modestbranding: 1, rel: 0, showInfo: 0 }; }
+                    console.log("videoUrl, title: ", videoUrl, title);
+                    $ui.popup(template, modal, null, { ctrl: _this, data: { videoUrl: videoUrl, playerVars: playerVars, title: title } });
+                };
+                service.init = function () {
+                    return service;
+                };
+                return service.init();
+            };
+            this.$get.$inject = ['$rootScope', 'youtubeEmbedUtils', '$ui'];
+        }
+        return AngularYouTubePopup;
+    }());
+    Minute.AngularYouTubePopup = AngularYouTubePopup;
+    var YoutubeLink = (function () {
+        function YoutubeLink($compile, $timeout, $youtube, $ui) {
+            var _this = this;
+            this.$compile = $compile;
+            this.$timeout = $timeout;
+            this.$youtube = $youtube;
+            this.$ui = $ui;
+            this.restrict = 'A';
+            this.link = function ($scope, element, attrs) {
+                element.click(function (event) {
+                    var href = element.attr('href');
+                    if (/youtube\.com/i.test(href)) {
+                        _this.$youtube.popup(href, element.text());
+                        event.stopImmediatePropagation();
+                        return false;
+                    }
+                });
+            };
+        }
+        YoutubeLink.factory = function () {
+            var directive = function ($compile, $timeout, $youtube, $ui) { return new YoutubeLink($compile, $timeout, $youtube, $ui); };
+            directive.$inject = ["$compile", "$timeout", "$youtube", "$ui"];
+            return directive;
+        };
+        return YoutubeLink;
+    }());
+    Minute.YoutubeLink = YoutubeLink;
+    angular.module('AngularYouTubePopup', ['MinuteFramework', 'youtube-embed'])
+        .directive('youtubeLink', YoutubeLink.factory())
+        .provider("$youtube", AngularYouTubePopup);
+})(Minute || (Minute = {}));
---
+++ src/js/angular-youtube-popup.ts
@@ -0,0 +1,69 @@
+/// <reference path="../../../minute/_all.d.ts" />
+module Minute {
+    export class AngularYouTubePopup implements ng.IServiceProvider {
+        constructor() {
+            this.$get.$inject = ['$rootScope', 'youtubeEmbedUtils', '$ui'];
+        }
+
+        $get = ($rootScope: ng.IRootScopeService, youtubeEmbedUtils: any, $ui: any) => {
+            let service: any = {};
+            let template = `
+                <div class="box box-lg" ng-init="data.loadVideo()">
+                    <div class="box-header with-border">
+                        <b class="pull-left"><span translate="">{{data.title || 'Video player'}}</span></b>
+                        <a class="pull-right close-button" href=""><i class="fa fa-times"></i></a>
+                        <div class="clearfix"></div>
+                    </div>
+        
+                    <form class="form-horizontal">
+                        <div class="box-body">
+                            <div class="embed-responsive embed-responsive-16by9">
+                                 <youtube-video class="embed-responsive-item" video-url="data.videoUrl" player-vars="data.playerVars"></youtube-video>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            `;
+
+            service.popup = (videoUrl, title = '', modal = false, playerVars = {autoplay: 1, controls: 0, modestbranding: 1, rel: 0, showInfo: 0}) => {
+                console.log("videoUrl, title: ", videoUrl, title);
+                $ui.popup(template, modal, null, {ctrl: this, data: {videoUrl: videoUrl, playerVars: playerVars, title: title}});
+            };
+
+            service.init = () => {
+                return service;
+            };
+
+            return service.init();
+        }
+    }
+
+    export class YoutubeLink implements ng.IDirective {
+        restrict = 'A';
+
+        constructor(private $compile: ng.ICompileService, private $timeout: ng.ITimeoutService, private $youtube: any, private $ui: any) {
+        }
+
+        static factory(): ng.IDirectiveFactory {
+            let directive: ng.IDirectiveFactory = ($compile: ng.ICompileService, $timeout: ng.ITimeoutService, $youtube: any, $ui: any) => new YoutubeLink($compile, $timeout, $youtube, $ui);
+            directive.$inject = ["$compile", "$timeout", "$youtube", "$ui"];
+            return directive;
+        }
+
+        link = ($scope: any, element: ng.IAugmentedJQuery, attrs: ng.IAttributes) => {
+            element.click((event) => {
+                let href = element.attr('href');
+
+                if (/youtube\.com/i.test(href)) {
+                    this.$youtube.popup(href, element.text());
+                    event.stopImmediatePropagation();
+                    return false;
+                }
+            });
+        }
+    }
+
+    angular.module('AngularYouTubePopup', ['MinuteFramework', 'youtube-embed'])
+        .directive('youtubeLink', YoutubeLink.factory())
+        .provider("$youtube", AngularYouTubePopup);
+}
